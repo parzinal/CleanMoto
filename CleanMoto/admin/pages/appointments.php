c6fdf40b574e257120b737c846675a99c074bd30
AldrianLoberiano <loberianorian@gmail.com>
2026-04-10 08:37:05 +0800

    Update appointments.php

--- CleanMoto/admin/pages/appointments.php
+++ CleanMoto/admin/pages/appointments.php
@@ -41,3 +41,4 @@ if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQU
     // Custom date range
     if (!empty($dateFrom) && !empty($dateTo)) {
         $whereConditions[] = "DATE(a.appointment_date) BETWEEN :dateFrom AND :dateTo";
+        $params[':dateFrom'] = $dateFrom;
