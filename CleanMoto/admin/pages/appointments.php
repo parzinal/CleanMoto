3093b063201e4b999c827a0e3ca2402fe3703cdd
AldrianLoberiano <loberianorian@gmail.com>
2026-04-10 08:37:30 +0800

    Update appointments.php

--- CleanMoto/admin/pages/appointments.php
+++ CleanMoto/admin/pages/appointments.php
@@ -53,3 +53,4 @@ if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQU
     
     // Search filter
     if (!empty($searchQuery)) {
+        $whereConditions[] = "(a.full_name LIKE :search OR a.contact LIKE :search2 OR a.id LIKE :search3)";
