546f079d22a73691cfb9201bdccf0f1d28dceec9
AldrianLoberiano <loberianorian@gmail.com>
2026-04-10 08:39:16 +0800

    Update appointments.php

--- CleanMoto/admin/pages/appointments.php
+++ CleanMoto/admin/pages/appointments.php
@@ -94,3 +94,4 @@ if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQU
     
     // Calculate filtered revenue
     $filteredRevenue = 0;
+    foreach ($appointments as $apt) {
