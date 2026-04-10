6a0ace0377ce2babfdc7751a12060d8eecd52c46
AldrianLoberiano <loberianorian@gmail.com>
2026-04-10 08:37:54 +0800

    Update appointments.php

--- CleanMoto/admin/pages/appointments.php
+++ CleanMoto/admin/pages/appointments.php
@@ -63,3 +63,4 @@ if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQU
     
     $appointments = [];
     $statusCounts = ['all' => 0, 'pending' => 0, 'confirmed' => 0, 'in_progress' => 0, 'completed' => 0, 'cancelled' => 0];
+    
